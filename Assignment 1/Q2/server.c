9653f58ed2634c5cce606f7f3114678869878359
Tatvam <td14@iitbbs.ac.in>
2019-01-16 02:01:03 +0530

    --

--- Assignment 1/Q2/server.c
+++ Assignment 1/Q2/server.c
@@ -1,55 +1,116 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <string.h>
+#include <fcntl.h>
 #include <netinet/in.h>
+#include <unistd.h>
+
 #define PORT 8115
+#define Max_Client 10
 
-int main(){
-    char *s = "Hello from server";
-    int opt = 1;
-    struct sockaddr_in sockinfo;
-    int sock_fd;
+char *s = "Hello from server";
+struct sockaddr_in sockinfo;
+struct timeval tv;
+int sock_fd;
+int opt = 1;
+int client_list[Max_Client] = {0};
 
-    if((sock_fd = socket(AF_INET,SOCK_STREAM,0)) < 0){
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
+int list_socket(int *sock_fd){
+
+    if((*sock_fd = socket(AF_INET,SOCK_STREAM,0))<0){
+        perror("Socket()");
+        return -1;
     }
-
     sockinfo.sin_family = AF_INET;
     sockinfo.sin_port = htons(PORT);
     sockinfo.sin_addr.s_addr = INADDR_ANY;
     memset(&(sockinfo.sin_zero),'\0',8);
 
-    if( setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,&opt, sizeof(opt)) < 0 )
+    if(setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,&opt, sizeof(opt))<0)
     {
         perror("setsockopt failed");
-        exit(EXIT_FAILURE);
+        return -1;
     }
 
-    if(bind(sock_fd,(struct sockaddr *)&sockinfo,sizeof(sockinfo))<0){
-        perror("Binding failed");
-        exit(EXIT_FAILURE);
+    if(bind(*sock_fd,(struct sockaddr *)&sockinfo,sizeof(sockinfo))<0){
+        perror("Bind()");
+        return -1;
     }
 
+    if(listen(*sock_fd,10)<0){
+        perror(Listen());
+        return -1;
+    }
+    
+    printf("Accepting connections on port %d.\n", (int)PORT);
+}
+void build_fd_sets(fd_set *readfds,fd_set *writefds,fd_set *exceptfds){
 
-    if(listen(sock_fd,5)<0){
-        perror("Failed");
-        exit(EXIT_FAILURE);
-    }
-    int new_fd;
-    int info_size = sizeof(sockinfo);
-    if((new_fd = accept(sock_fd,(struct sockaddr *)&sockinfo,&info_size))<0){
-         perror("Failed");
-        exit(EXIT_FAILURE);
-    }
-    char buffer[1000];
-    char rec[1000] = {0};
-    read(new_fd,rec,1000);
-    printf("%s\n",rec);
-    printf("Input the data you have to send to client\n");
-    scanf(" %[^\n]",buffer);
-    send(new_fd,buffer,sizeof(buffer),0);
+    FD_ZERO(readfds);
+    FD_SET(STDIN_FILENO,readfds);
+    FD_SET(sock_fd,readfds);
 
 }
+int main(){
+  
+    if(list_socket(&sock_fd)!=0){
+        exit(EXIT_FAILURE);
+    }
+    // Non Blocking STD_IN 
+    fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
+
+    fd_set readfds;
+    fd_set writefds;
+    fd_set exceptfds;
+
+    int mx_fd = sock_fd;
+
+    tv.tv_sec = 10;
+    tv.tv_usec = 500000;
+
+    printf("Waiting for incoming connections.\n");
+
+    while(1){
+        build_fd_sets(&readfds,&writefds,&exceptfds);
+        int new_fd;
+        int info_size = sizeof(sockinfo);
+
+        int sel = select(mx_fd+1,&readfds,NULL,NULL,&tv);
+
+        if(sel == -1){
+            perror("Select()");
+        }else if(sel == 0){
+            printf("Timeout Occured");
+        }else{
+            if(FD_ISSET(sock_fd,&readfds)){
+                if((new_fd = accept(sock_fd,(struct sockaddr *)&sockinfo,&info_size))<0){
+                    perror("Accept()");
+                    exit(EXIT_FAILURE);
+                }
+                 printf("New connection , socket fd is %d , ip is : %s , port : %d\n" , new_fd , inet_ntoa(sockinfo.sin_addr) , ntohs (sockinfo.sin_port)); 
+
+                if(send(new_fd,s,sizeof(s),0)!=strlen(s)){
+                    perror("Send()");
+                }
+
+                 puts("Welcome message sent successfully");   
+
+                 int i;
+                 for(i = 0;i<Max_Client;i++){
+                     if(client_list[i]==0){
+                         client_list[i]=new_fd;
+                         break;
+                     }
+                 }
+             }
+
+             if()
+         }
+     }
+
+    }
+
